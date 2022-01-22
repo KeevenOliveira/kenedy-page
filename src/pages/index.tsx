f6bf3c5610848798594bec1fb62e81aaec15468d
Keeven Oliveira <66172398+KeevenOliveira@users.noreply.github.com>
2022-01-22 01:20:30 -0300

    Creating Floating Button WhatsApp

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,9 +4,10 @@ import Cards from '../components/Cards';
 import CarouselMain from '../components/Carousel';
 import Head from 'next/head'
 // import Image from 'next/image'
-import { Container } from '../styles';
+import { Container, FloatingWhatsApp } from '../styles';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
+import { FaWhatsapp } from 'react-icons/fa';
 
 const Home: NextPage = () => {
   return (
@@ -17,6 +18,22 @@ const Home: NextPage = () => {
         <Cards />
         <Footer />
       </Container>
+      <a href="https://api.whatsapp.com/send?phone=5581982687667&text=Ol%C3%A1%2C%20tudo%20bem%20Kennedy%3F%20Gostaria%20de%20saber%20mais%20informa%C3%A7%C3%B5es%20sobre%20os%20apartamento%20da%20direcional%20engenharia." style={{
+        width: '50px',
+        height: '50px',
+        position: 'fixed',
+        borderRadius: '20px',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        zIndex: 1000,
+        backgroundColor: '#00E676',
+        right: '20px',
+        bottom: '40px',
+        boxShadow: '1px 1px 2px #888'
+      }}>
+        <FaWhatsapp size={30} />
+      </a>
     </>
   )
 }
--- src/styles/index.ts
+++ src/styles/index.ts
@@ -9,3 +9,18 @@ export const Title = styled.h1`
     font-weight: 600;
     margin-bottom: 0.5em;
 `;
+
+export const FloatingWhatsApp = styled.a`
+    width: 50px,
+    height: 50px,
+    position: fixed,
+    border-radius: 20px,
+    display: flex,
+    justify-content: center,
+    align-items: center,
+    z-index: 1000,
+    background-color: #00E676,
+    right: 20px,
+    bottom: 40px,
+    box-shadow: 1px 1px 2px #888
+`;
